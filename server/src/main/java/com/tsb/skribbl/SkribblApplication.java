dedd6b2c26680621123cb1e4e91a9fc06dd1d5fe
Angel Penchev <angel.penchev.tsb@gmail.com>
2021-04-05 21:06:07 +0300

    [Feat] Client + Server init
    Initialized the Spring server and the React client.

---
+++ server/src/main/java/com/tsb/skribbl/SkribblApplication.java
@@ -0,0 +1,13 @@
+package com.tsb.skribbl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkribblApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkribblApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/tsb/skribbl/SkribblApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsb.skribbl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkribblApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
